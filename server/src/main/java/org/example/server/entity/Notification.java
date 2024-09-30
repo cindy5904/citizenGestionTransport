898b247f2d7fbb3443a7b74690f0d4660087cb45
Lalia SY <laliasy@yahoo.fr>
2024-09-30 16:49:53 +0200

    sauvegarde entities

--- server/src/main/java/org/example/server/entity/Notification.java
+++ server/src/main/java/org/example/server/entity/Notification.java
@@ -1,4 +1,8 @@
 package org.example.server.entity;
 
 public class Notification {
+    /*
+    @ManyToOne
+    @JoinColumn(name = "admin-id", nullable = false)
+    private Admin admin;*/
 }
--- server/src/main/java/org/example/server/entity/TransportLine.java
+++ server/src/main/java/org/example/server/entity/TransportLine.java
@@ -1,4 +1,8 @@
 package org.example.server.entity;
 
 public class TransportLine {
+    /*  @ManyToMany(mappedBy = "stations")
+    private Set<TransportLine> transportLines;
+    * /
+     */
 }
